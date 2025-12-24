c017abfb65929a9e13aba800d8d0f4865033757a
hans <hans.tiwari@gmail.com>
2025-12-25 04:19:21 +0530

    update all

--- script.js
+++ script.js
@@ -1,15 +1,2 @@
-// Greeting
-const greetingEl = document.getElementById("greeting");
-const hour = new Date().getHours();
-
-let greeting = "Welcome, Guest 👋";
-
-if (hour < 12) greeting = "Good morning, Guest 🌅";
-else if (hour < 18) greeting = "Good afternoon, Guest ☀️";
-else greeting = "Good evening, Guest 🌙";
-
-greetingEl.textContent = greeting;
-
-// Footer year
 document.getElementById("year").textContent =
   new Date().getFullYear();
